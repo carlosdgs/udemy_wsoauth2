7b7119c07e0b28423b06d15db0ee9a6fc70a4f53
Carlos Delfeu <44553198+carlosdgs@users.noreply.github.com>
2020-02-26 23:42:41 -0300

    Add files via upload
    - Nesta etapa 
      Mongo DB compass já configurado com DB : wsoauth2.user
      Serviço já trazendo dados incluidos no documento mongo

---
+++ src/main/java/com/hugosilva/curso/ws/resources/UserResource.java
@@ -0,0 +1,35 @@
+package com.hugosilva.curso.ws.resources;
+
+
+import com.hugosilva.curso.ws.domain.User;
+import com.hugosilva.curso.ws.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+//        List<User> users = new ArrayList<>();
+//        User joao = new User("João", "Souza", "joao@gmail.com");
+//        User maria = new User("Maria", "Teixeira", "maria@gmail.com");
+//        users.addAll(Arrays.asList(joao, maria));
+        return ResponseEntity.ok().body(users);
+    }
+}
+
+
+
